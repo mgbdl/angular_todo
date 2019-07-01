382a31c05c02fb15af4b2122ecf96633d4d56022
Miguel Breceda <miguel.breceda@outlook.es>
2019-07-01 09:23:08 -0500

    first commit

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+import { Todo } from 'src/app/models/Todos';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos: Todo[];
+
+  constructor(
+    private todoService:TodoService, 
+  ) {}
+
+  ngOnInit() {
+    this.todoService.getTodos().subscribe(todos => {
+      this.todos = todos;
+    });
+  }
+
+  deleteTodo(todo:Todo){
+    // Remove From UI
+    this.todos = this.todos.filter(t => t.id !== todo.id)
+    // Remove From Server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
+  addTodo(todo:Todo) {
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.unshift(todo);
+    })
+  }
+}
